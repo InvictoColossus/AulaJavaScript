f64a4ea995833bb614e8d5e9798b839db8b61df0
Gedalias Tarcisio <75184726+InvictoColossus@users.noreply.github.com>
2021-06-24 10:43:13 -0300

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,29 @@
+  
+                                 /*Array
+var fruta = [{nome:"maça", cor:"vermelha"},{nome:"maça", cor:"vermelha"}]   // lista e dicionario
+var fruta = {nome:"maça", cor:"vermelha"}                           //Dicionário
+console.log(fruta);
+alert(fruta[1].nome);
+                                  
+var lista = ["maça","pera","laranja"]    //lista
+lista.push("acerola")                       //adiciona
+lista.pop();                                //tira p ultimo/console.log(lista[0]);  //length=tamanho.  reverse=tras elementos ao contrário tostring vira string
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));               //join junta coloca qualquer coisa
+alert(lista[2])                           //imprimi só a laranja  
+*/
+/*
+var nome = "Gedalias tarcisio";
+var idade= 29;
+var idade2= 10;
+var frase=" japão é o melhor time do mundo";
+alert( nome +  "  tem  "+ idade +" anos ");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade);     
+console.log(frase.replace("japão", "França"));   //replace muda a variável por outra japão por frança
+alert(frase.replace("japão","nigéria"));
+console.log(frase.toUpperCase());                //maiúsculo tolowercase=minúsculo
+*/
+
+
