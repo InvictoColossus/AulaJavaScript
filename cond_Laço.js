f97dee560f406f19fe6043ab11b42aba89646ebc
Gedalias Tarcisio <75184726+InvictoColossus@users.noreply.github.com>
2021-06-24 14:08:15 -0300

    Add files via upload

---
+++ cond_Laço.js
@@ -0,0 +1,48 @@
+/*
+var d = new Date();
+alert(d.getDay());          //get mostra minutos só   mes etc.
+alert(d.getHours())
+alert(d.getMonth()+1)
+
+var count;
+for(count=0;count<5;count++){
+alert(count);
+};
+
+var idade =0;
+while (idade < 5){
+    console.log(idade);
+    alert(idade)
+    idade++;
+};
+
+var idade = prompt("Qual a sua idade?")         //prompt pgt
+if (idade >= 18){
+    alert("Maior de idade");                  
+}else{
+    alert(" menor de idade");
+ };
+*/
+                 /*Funções
+function soma(n1,n2){
+    return n1 + n2;
+}
+    alert(soma(10,12));
+/*
+function validaIdade(idade){
+    var idade;
+    if (idade>= 18){
+        validar=true
+    }else{
+        validar=false
+    }
+    return validar;
+} 
+var idade=prompt("Qual a sua idade?")
+console.log(validaIdade(idade));
+*/
+
+function clicou(){
+      alert("obrigado por Clicar");
+
+}
